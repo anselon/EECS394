270aab733de7fd6594b0d496aabf423c61a3a9e0
Anna Selonick <abselonick@gmail.com>
2014-10-23 19:37:49 -0500

    update

---
+++ scripts/main.js
@@ -0,0 +1,183 @@
+$("#leftbutton").attr('data-theme', "e");
+$("#rightbutton").attr('data-theme', "e");
+
+
+
+//$( "img.flyer" ).attr('src', Parse.flyers.get('hard_link').where
+
+
+var flyer_index = 0;
+getInstruction();
+
+function getInstruction(){
+	if (Parse.User.current().get("instruction") == false) {
+		$("body").append("<img id='instruction' src='./instruction.png' width='100%' style='position: fixed; z-index:21'/>");
+		$("body").append("<div class='overlay'></div> ")
+		$("#instruction").on('click',function(event){
+			this.remove();
+			$(".overlay").remove();
+			Parse.User.current().set("instruction", true);
+			Parse.User.current().save();
+		});
+	}
+}
+ 
+
+
+//add flyer_index as event data
+function getBookmarked(){
+	// Bind the swipeHandler callback function to the swipe event on div.box
+
+	$user = Parse.User.current();
+
+	$(".flyer").hide();//http://localhost:8888/flyers/flyer7.jpg
+	$(".bookmark").show();
+	if($user.get('bookmark').length > 0){
+		for(i =0 ; i < $user.get('bookmark').length; ++i){
+			$(".bookmark").append('<img class = "favorites" src = "flyers/' + $user.get('bookmark')[i] +'" />');
+		}
+	}else{
+		alert('You have not bookmarked anything!');
+	}
+		
+
+	
+	
+
+ }
+	
+
+
+function getHome(){
+	// Bind the swipeHandler callback function to the swipe event on div.box
+
+   $(".flyer").show();
+   $(".bookmark").html('');
+		$( "img.flyer" ).bind( "swipe", { sort: "image"}, swipeHandler );
+
+		flyer_index = 0;
+
+ }		
+
+jQuery( "img.flyer" ).on( "swipeleft", function( event ) { 
+	console.log('SWIPE LEFT');
+
+	flyer_index = flyer_index + 1;
+	//query for next flyer
+	var flyers = Parse.Object.extend("flyers");
+	var query = new Parse.Query(flyers);
+	var flyer_link;
+	query.equalTo('flyer_num', flyer_index);
+	query.find({
+	  success: function(results) {
+	    //Success callback
+		var object = results[0];
+		if (results.length == 0) {
+			flyer_index = flyer_index -1;
+			console.log("no index");
+			console.log(flyer_index);
+		} else {
+	     	flyer_link = object.get('hard_link');
+	     	$(event.target).attr("src", "flyers/" + flyer_link.substring(1, flyer_link.length-1));  
+
+	     	if ( object.get('freefood') == true ) {$("img.flyer").css({ "border": "5px solid green" });} 
+	     	else {$("img.flyer").css({ "border": "0px" });} 	      
+     	}
+	  },
+	  error: function(error) {
+	  	flyer_index = flyer_index -1;
+	    console.log(error);
+	  }
+	});
+
+ })
+
+jQuery( "img.flyer" ).on( "swiperight", function( event ) { 
+	console.log('SWIPE RIGHT');
+
+	flyer_index = flyer_index - 1;
+	//query for next flyer
+	var flyers = Parse.Object.extend("flyers");
+	var query = new Parse.Query(flyers);
+	var flyer_link;
+	query.equalTo('flyer_num', flyer_index);
+	query.find({
+	  success: function(results) {
+	    //Success callback
+		var object = results[0];
+		if (results.length == 0) {
+			flyer_index = flyer_index +1;
+			console.log("no index");
+			console.log(flyer_index);
+		} else {
+			flyer_link = object.get('hard_link');
+	     	$(event.target).attr("src", "flyers/" + flyer_link.substring(1, flyer_link.length-1));  
+
+	     	if ( object.get('freefood') == true ) {$("img.flyer").css({ "border": "5px solid green" });} 
+	     	else {$("img.flyer").css({ "border": "0px" });} 
+		}	      
+
+	  },
+	  error: function(error) {
+		flyer_index = flyer_index +1;
+	    console.log(error);
+	  }
+	});
+
+} )
+
+
+
+
+
+//go to next flyer
+//calls swipeHandler
+function nextFlyer(){
+	$( "img.flyer" ).trigger("swipeleft");
+
+	$("#rightbutton").removeClass("ui-btn-active");
+	$("#leftbutton").removeClass("ui-btn-active");
+}
+	
+$('#leftbutton').click(function(){
+
+	$("#leftbutton").toggleClass("ui-btn-active");
+    
+	window.setTimeout(nextFlyer, 500);
+});
+
+$('#rightbutton').click(function(){
+
+
+	$("#rightbutton").toggleClass("ui-btn-active");
+
+
+
+  	$flyerSlug = $('img.flyer').attr("src").substring(7);
+  	$user = Parse.User.current();
+
+	$user.addUnique('bookmark', $flyerSlug);
+	$user.save;
+
+  window.setTimeout(nextFlyer, 500);
+
+
+});
+
+
+
+
+// Upload function stuff
+$(document).ready(function(){
+	console.log('read');
+	$(".upload").hide();
+	$("#upload").click(function(){
+		$(".upload").show();
+		console.log("hi");
+	})
+	$("#goback").click(function(){
+		$(".upload").hide();
+	})
+
+});
+
---
+++ scripts/upload.js
@@ -0,0 +1,80 @@
+  $(function() {
+    $( "#datepicker" ).datepicker();
+    console.log('test');
+  });
+  $(function() {
+    $( "#datepicker2" ).datepicker();
+  });
+
+  Parse.initialize("sH4OGJXJIgXqfnuAuLWFNWYvfhN7pNQjDmkpWaSw", "RwtmRovMtfB67Oq9PV7Y9mbEb7nZgnQFShFB38w8");
+  $(function() {
+    var file;
+
+    // Set an event listener on the Choose File field.
+    $('#fileselect').bind("change", function(e) {
+      var files = e.target.files || e.dataTransfer.files;
+      // Our file var now holds the selected file
+      file = files[0];
+    });
+
+    // This function is called when the user clicks on Upload to Parse. It will create the REST API request to upload this image to Parse.
+    $('#uploadbutton').click(function() {
+      var serverUrl = 'https://api.parse.com/1/files/' + file.name;
+
+      $.ajax({
+        type: "POST",
+        beforeSend: function(request) {
+          request.setRequestHeader("X-Parse-Application-Id", 'sH4OGJXJIgXqfnuAuLWFNWYvfhN7pNQjDmkpWaSw');
+          request.setRequestHeader("X-Parse-REST-API-Key", 'DKj5TwAPMRqpiGHSDbWavpEYQebFnr4GnNBnnI1Y');
+          request.setRequestHeader("Content-Type", file.type);
+        },
+        url: serverUrl,
+        data: file,
+        processData: false,
+        contentType: false,
+        success: function(data) {
+
+          var newFlyer = Parse.Object.extend("flyers");
+          var flyernew = new newFlyer();
+          flyernew.set("hard_link", data.url);
+          var d = $('#datepicker').datepicker('getDate');
+          var d2 = $('#datepicker2').datepicker('getDate');
+          if ($('#myCheck').is(':checked')) {
+            var check = true;
+          } else {
+            var check = false;
+          } 
+          /*
+          var query = new Parse.Query('flyers');
+query.count({
+  success: function(number) {
+    alert:('count success');
+    flyernew.set("flyer_num", number);
+    // There are number instances of MyClass.
+  },
+
+  error: function(error) {
+    // error is an instance of Parse.Error.
+  }
+}); */
+          flyernew.set("start", d);
+          flyernew.set("end", d2);
+          flyernew.set("freefood", check);
+          flyernew.save(null, {
+            success: function(flyernew) {alert('success')},
+            error: function(flyernew, error) {alert('fail')}});
+
+
+
+
+
+        },
+        error: function(data) {
+          var obj = jQuery.parseJSON(data);
+          alert(obj.error);
+        }
+      });
+    });
+
+
+  });
