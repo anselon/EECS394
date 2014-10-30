3b24db6d2e7c1e7e788233a6c24e3d85c7530530
Edward Hu <iedwardhu@gmail.com>
2014-10-29 22:49:40 -0500

    undid commit, caused a bug

--- scripts/main.js
+++ scripts/main.js
@@ -40,14 +40,13 @@ function getBookmarked(){
 	$(".bookmark").show();
 
 	var bookmarks = JSON.parse(localStorage.getItem('bookmark_flyers'));
-	console.log("length is  " +  bookmarks.length);
+
 	if (bookmarks != null){
-		if(bookmarks.length > 0){
-			for(i =0 ; i < bookmarks.length; ++i){
-				$("#favorites-btn").append('<img class = "favorites" src = "flyers/' + bookmarks[i] +'" />');
-				console.log('hi"0;');
-			}
+	if(bookmarks.length > 0){
+		for(i =0 ; i < bookmarks.length; ++i){
+			$(".bookmark").append('<img class = "favorites" src = "flyers/' + bookmarks[i] +'" />');
 		}
+	}
 
 	}
 	//hide bookmark button
